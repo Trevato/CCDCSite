7e71a5e77d860f939b5be3efe10669da80a4eebf
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-02-13 13:38:30 -0600

    Adding Debian db.

--- ccdcApp/settings.py
+++ ccdcApp/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'ccdcApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "frog",
+        "USER": "root",
+        "PASSWORD": "College2020",
+        "HOST": "172.20.240.20",
+        "PORT": "3306",
     }
 }
 
