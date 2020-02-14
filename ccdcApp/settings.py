408b2197e8c30b1e1eedca233b328c01bb9575d9
Trevor Dobbertin <trevordobbertin@gmail.com>
2020-02-14 12:57:34 -0600

    Change module name.

--- ccdcApp/settings.py
+++ ccdcApp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'CCDCSite.apps.CcdcappConfig',
+    'CCDCApp.apps.CcdcappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
